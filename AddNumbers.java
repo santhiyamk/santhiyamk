e8fc562ab7ee84e2838e4d5442188d61d33f223f
santhiyamk <108972758+santhiyamk@users.noreply.github.com>
2024-01-11 12:45:58 +0530

    Files added

---
+++ AddNumbers.java
@@ -0,0 +1,9 @@
+public class AddNumbers{
+	public static void main(){
+	int number1 = 5, number2 =10;
+	int sum = number1+number2;
+	System.out.println(sum);
+	}
+}
+
+	
---
+++ OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven{
+	public static void main(){
+	int number = 0;
+	if(number % 2 == 0)
+		System.out.println(number + "is even");
+	else
+		System.out.println(number + "is Odd");
+	}
+}
